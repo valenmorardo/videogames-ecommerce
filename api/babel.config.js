52e13b17035fcb188f2f3af270bcf64c8c2af85b
Valentin <valentin.morardo@gmail.com>
2023-12-02 19:36:55 -0300

    cfg para el api backend ready

---
+++ api/babel.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 'current',
+				},
+			},
+		],
+		'@babel/preset-typescript',
+	],
+	plugins: [
+		[
+			'module-resolver',
+			/* {
+				alias: {
+					'@config': './src/config',
+					'@controllers': './src/controllers',
+					'@routes': './src/routes',
+					'@models': './src/models',
+					'@libs': './src/libs/',
+					'@middlewares': './src/middlewares',
+					'@utils': './src/utils/',
+				},
+			}, */
+		],
+	],
+};
