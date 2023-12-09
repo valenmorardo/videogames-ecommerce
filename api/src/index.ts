3a71d928dbf2129e8c8588b6bad05628cd04f8b0
Valentin <valentin.morardo@gmail.com>
2023-12-08 21:19:38 -0300

    DBUtils function added

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,12 +9,12 @@
 //		▄███████▄.▲.▲▲▲▲▲▲▲▲
 //		████████████████████▀▀
 
-import startServer from "@config/server/startServer";
+// START SERVER FUNCTION
+import startServer from '@config/server/startServer';
 
 const runApp = async (): Promise<boolean> => {
 	try {
 		await startServer();
-		
 	} catch (err) {
 		console.log('ERROR!!');
 		if (typeof err === 'string') {
@@ -28,3 +28,24 @@ const runApp = async (): Promise<boolean> => {
 	return true;
 };
 runApp();
+//!!!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// DB functions utils
+import { cleanDataInDB, mainTest } from '@config/DBUtils';
+
+const DBUtils = async (): Promise<boolean> => {
+	try {
+		/* await cleanDataInDB(); */
+		await mainTest();
+	} catch (err) {
+		console.log('ERROR!!');
+		if (typeof err === 'string') {
+			throw new Error(err);
+		} else {
+			console.log(err);
+			throw new Error('An unknown error occurred');
+		}
+	}
+	return true;
+};
+DBUtils()
