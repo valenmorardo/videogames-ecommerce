4372cabfce43f9e1738f3b414055659757e8af58
Valentin <valentin.morardo@gmail.com>
2024-02-04 17:43:33 -0300

    graphql private cfg added

---
+++ api/src/graphql/private/index.ts
@@ -0,0 +1,14 @@
+const typeDefs = `
+    type Query {
+        welcome: String
+    }
+`;
+
+const resolvers = {
+	Query: {
+        welcome: () => {
+            return 'Welcome'
+        }
+    },
+};
+export { typeDefs, resolvers };
