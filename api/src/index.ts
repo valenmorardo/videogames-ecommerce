a68840f11f20a9f6bf77f901282715234fa8625c
Valentin <valentin.morardo@gmail.com>
2024-02-04 17:44:39 -0300

    apollo servers start functions added

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,14 +10,22 @@
 //		████████████████████▀▀
 
 // START SERVER/APP FUNCTION
-import { expressMiddleware } from '@apollo/server/express4';
 import app from './app/app';
-import apolloServer from './app/apolloServer';
+import apolloServerPublic from './app/apolloServerPublic';
+import apolloServerPrivate from './app/apolloServerPrivate';
+import apolloServerAdmin from './app/apolloServerAdmin';
+import { expressMiddleware } from '@apollo/server/express4';
 
 const startApp = async () => {
 	try {
-		await apolloServer.start();
-		app.use('/graphql', expressMiddleware(apolloServer));
+		await apolloServerPublic.start();
+		await apolloServerPrivate.start();
+		await apolloServerAdmin.start();
+
+		app.use('/graphql-public', expressMiddleware(apolloServerPublic));
+		app.use('/graphql-private', expressMiddleware(apolloServerPrivate));
+		app.use('/graphql-admin', expressMiddleware(apolloServerAdmin));
+
 		app.listen(app.get('port'));
 	} catch (error) {
 		console.log(error);
@@ -26,8 +34,22 @@ const startApp = async () => {
 			message: 'Error at startApp function.',
 		};
 	}
-	console.log(`Express ready at: http://localhost:${app.get('port')}`);
-	console.log(`GraphQL ready at: http://localhost:${app.get('port')}/graphql`);
+	console.log(`🚀 Express ready at: http://localhost:${app.get('port')}`);
+	console.log(
+		`✅ GraphQL public service ready at: http://localhost:${app.get(
+			'port',
+		)}/graphql-public`,
+	);
+	console.log(
+		`🌐 GraphQL private service ready at: http://localhost:${app.get(
+			'port',
+		)}/graphql-private`,
+	);
+	console.log(
+		`🔒 GraphQL admin service ready at: http://localhost:${app.get(
+			'port',
+		)}/graphql-admin`,
+	);
 
 	return true;
 };
