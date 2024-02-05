884918dc6535a107d850dfd799817adaea93c2b8
Valentin <valentin.morardo@gmail.com>
2024-02-05 17:44:59 -0300

    allControllers added for export

---
+++ api/src/controllers/index.controllers.ts
@@ -0,0 +1,11 @@
+import expressMiddlewarePublicApolloSv from "./apollo/expressMiddlewarePublicApolloSv";
+import expressMiddlewarePrivateApolloSv from "./apollo/expressMiddlewarePrivateApolloSv";
+import expressMiddlewareAdminApolloSv from "./apollo/expressMiddlewareAdminApolloSv";
+
+const controllers = {
+    expressMiddlewarePublicApolloSv,
+    expressMiddlewarePrivateApolloSv,
+    expressMiddlewareAdminApolloSv
+}
+
+export default controllers
