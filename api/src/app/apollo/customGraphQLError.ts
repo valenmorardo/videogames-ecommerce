29cce3f78bad508aa9ee83232d9047b20174aabe
Valentin <valentin.morardo@gmail.com>
2024-02-07 23:02:27 -0300

    custom graphQL Error class constructor & formattedGraphQLError interface added

---
+++ api/src/app/apollo/customGraphQLError.ts
@@ -0,0 +1,17 @@
+import { IAdditionalDataError } from "@libs/typings/additionalDataError";
+
+export class CustomGraphQLError {
+	message!: string;
+    error_code!: string;
+	error_status!: number;
+    error_path!: (string | number)[];
+	additional_data?: IAdditionalDataError;
+
+	constructor(message: string, error_code: string, error_status: number, error_path: (string | number)[], additional_data?: any) {
+		this.message = message
+        this.error_code = error_code
+		this.error_status = error_status
+        this.error_path = error_path
+		this.additional_data = additional_data
+	}
+}
