995afa0e696e9a6cf2fc6fb836f0c4f16333ff23
Valentin <valentin.morardo@gmail.com>
2024-02-15 18:32:46 -0300

    GraphQL-Service: index.ts main function startApp ready

---
+++ api/graphql-service/src/index.ts
@@ -0,0 +1,38 @@
+// 	   /\_/\
+//    ( o.o )
+// 	   > ^ <
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//		─────▄████▀█▄
+//		───▄█████████████████▄
+//		─▄█████.▼.▼.▼.▼.▼.▼▼▼▼
+//		▄███████▄.▲.▲▲▲▲▲▲▲▲
+//		████████████████████▀▀
+
+// START SERVER/APP FUNCTION
+import app from '@app/app';
+import myApolloServer from '@app/apollo/apolloServer';
+
+import errorHandler from '@utils/errorHandler';
+import { ICustomError } from '@libs/typings/customError';
+
+const startApp = async (): Promise<ICustomError | boolean> => {
+	try {
+		await myApolloServer.start();
+
+		app.listen(app.get('port'));
+
+		console.log(`🚀 Express ready at: http://localhost:${app.get('port')}`);
+		console.log(
+			`✅ GraphQL service ready at: http://localhost:${app.get(
+				'port',
+			)}/graphql`,
+		);
+
+		return true;
+	} catch (error) {
+		return errorHandler(error);
+	}
+};
+
+startApp();
