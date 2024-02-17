6f756b5b9a846c51b62c1c70d1f1b53731bb3675
Valentin <valentin.morardo@gmail.com>
2024-02-16 22:26:22 -0300

    GRAPHQL: userCreate resolver ready

--- api/graphql-service/src/graphql/resolvers/mutations/userCreate/UserCreate.resolver.ts
+++ api/graphql-service/src/graphql/resolvers/mutations/userCreate/UserCreate.resolver.ts
@@ -1,14 +1,14 @@
 //TYPES
-import { IResolverContext } from '@libs/typings/resolverContext';
+import { IResolverContext } from '@libs/typings/graphql/resolverContext';
 import { IUserAttributes } from '@DB/typings/userAttributes';
-import { InewUser } from '@graphql/typings/newUser';
-import { IformattedCustomGraphQLError } from '@libs/typings/formattedCustomGraphQLError';
 
 import prisma from 'src/DB';
 
-import newUserValidation from './newUserFields.JoiValidation';
+import newUserValidation from './helpers/newUser.JoiValidation';
+import encryptPassword from './helpers/encryptPassword';
+import { InewUser } from '@graphql/typings/newUser';
 
-import encryptPassword from './encryptPassword';
+import { GraphQLError } from 'graphql';
 
 const UserCreate = async (
 	_parent: any,
@@ -17,28 +17,27 @@ const UserCreate = async (
 	},
 	_context: IResolverContext,
 	_info: any,
-): Promise<IUserAttributes | IformattedCustomGraphQLError> => {
+): Promise<IUserAttributes | GraphQLError> => {
 	try {
-		//validate user and refactor fields
+
 		const userValidated = newUserValidation(args.input) as InewUser;
 
-		// password encrypted
-		const passwordEncrypted = (await encryptPassword(
+		const encryptedPassword = (await encryptPassword(
 			userValidated.password,
 		)) as string;
 
-		// user create and added to DB
 		const newUser = await prisma.user.create({
 			data: {
 				name: userValidated.name,
 				email: userValidated.email,
 				username: userValidated.username,
-				password: passwordEncrypted,
+				password: encryptedPassword,
 			},
 		});
 
 		return newUser;
 	} catch (error) {
+		console.log(error)
 		return error;
 	}
 };
