9bdd2236dcdde66f03fb7709e4033992890a9b0c
Valentin <valentin.morardo@gmail.com>
2024-02-20 16:12:52 -0300

    inputs for newUser and userLogin ready

--- api/graphql-service/src/graphql/typeDefs/inputs/index.inputs.ts
+++ api/graphql-service/src/graphql/typeDefs/inputs/index.inputs.ts
@@ -1,15 +1,26 @@
-import { readFileSync } from 'fs';
+import { read, readFileSync } from 'fs';
 import path from 'path';
 
 const UserCreate: string = readFileSync(
-	path.join(__dirname, './userCreate.type.graphql'),
+	path.join(__dirname, './userCreate.input.graphql'),
 	{
 		encoding: 'utf-8',
 	},
 );
 
-const Inputs = {
-	UserCreate
-}
+const UserLogin: string = readFileSync(
+	path.join(__dirname, './userLogin.input.graphql'),
+	{
+		encoding: 'utf-8',
+	},
+);
 
-export default Inputs
+
+
+const Inputs = {
+	UserCreate,
+	UserLogin,
+	
+};
+
+export default Inputs;
