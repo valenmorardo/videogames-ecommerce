2ac8a8629a48a702d4910cdcdc88f9ec6532d6c8
Valentin <valentin.morardo@gmail.com>
2024-02-20 16:20:35 -0300

    JWT env variables added

--- api/graphql-service/src/utils/varEnvironments.ts
+++ api/graphql-service/src/utils/varEnvironments.ts
@@ -1,9 +1,11 @@
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const env = {
-    PORT: process.env.PORT
-}
+	PORT: process.env.PORT,
+	JWT_SECRET: process.env.JWT_SECRET,
+	JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+};
 
-export default env
+export default env;
