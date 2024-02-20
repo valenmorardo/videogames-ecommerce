dde7ce27e1fab66ffc56a62c6fd0fdefc5ab1e02
Valentin <valentin.morardo@gmail.com>
2024-02-20 16:11:30 -0300

    loginResponse type imported and exported

--- api/graphql-service/src/graphql/typeDefs/types/index.types.ts
+++ api/graphql-service/src/graphql/typeDefs/types/index.types.ts
@@ -5,12 +5,16 @@ const User: string = readFileSync(path.join(__dirname, './user.graphql'), {
 	encoding: 'utf-8',
 });
 
-
-
+const LoginResponse: string = readFileSync(
+	path.join(__dirname, './loginResponse.graphql'),
+	{
+		encoding: 'utf-8',
+	},
+);
 
 const Types = {
-    User,
-}
+	User,
+    LoginResponse
+};
 
-
-export default Types
+export default Types;
