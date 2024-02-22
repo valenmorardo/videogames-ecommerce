22fbd2acc569425547cf4a85bf39cb9f774e8ff0
Valentin <valentin.morardo@gmail.com>
2024-02-22 20:56:21 -0300

    handler added function added to startApp function

--- api/graphql-service/src/index.ts
+++ api/graphql-service/src/index.ts
@@ -16,10 +16,13 @@ import myApolloServer from '@app/apollo/apolloServer';
 import errorHandler from '@utils/errorHandler';
 import { ICustomError } from '@libs/typings/customError';
 
+import handlerData from '@DB/handlerData';
+
 const startApp = async (): Promise<ICustomError | boolean> => {
 	try {
 		await myApolloServer.start();
 
+		await handlerData();
 		app.listen(app.get('port'));
 
 		console.log(`🚀 Express ready at: http://localhost:${app.get('port')}`);
