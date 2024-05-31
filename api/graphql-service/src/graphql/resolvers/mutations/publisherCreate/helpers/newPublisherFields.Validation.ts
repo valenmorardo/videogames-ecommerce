e54be49fd4296ba8f1091ba09e80751d6ea94cc4
Valentin <valentin.morardo@gmail.com>
2024-05-31 19:03:53 -0300

    publisher resolver refactor

--- api/graphql-service/src/graphql/resolvers/mutations/publisherCreate/helpers/newPublisherFields.Validation.ts
+++ api/graphql-service/src/graphql/resolvers/mutations/publisherCreate/helpers/newPublisherFields.Validation.ts
@@ -1,8 +1,41 @@
 
 import customGraphQLError from "@graphql/resolvers/services/errors/customGraphQLError"
 import { INewPublisher } from "@graphql/resolvers/typings/newPublisher"
-import newPublisherFieldsSchema from "./newPublisherFields.Schema"
 import httpErrors from "@graphql/resolvers/services/errors/httpErrors/index.errors"
+
+import Joi from 'joi';
+import { ObjectSchema } from 'joi';
+
+const newPublisherFieldsSchema: ObjectSchema<INewPublisher> = Joi.object({
+	userId: Joi.string()
+		.required()
+		.guid({
+			version: ['uuidv4'],
+			separator: '-',
+		}),
+
+	name: Joi.string()
+		.required()
+		.pattern(/^[a-zA-Z0-9\s]+$/)
+		.trim()
+		.custom((value) => {
+			return value.replace(/\s+/g, ' ');
+		}),
+
+	profilePhoto: Joi.string()
+		.trim()
+		.custom((value): boolean | Error => {
+			const onlyImgUrl =
+				/^(https?|ftp):\/\/[^\s/$.?#].[^\s]*\.(jpg|jpeg|png|gif|bmp)$/;
+
+			if (onlyImgUrl.test(value)) {
+				throw new Error('La url no es valida.');
+			}
+			return true;
+		}),
+});
+
+
 
 const newPublisherFieldsValidation = (newPublisher: INewPublisher ) => {
     try {
