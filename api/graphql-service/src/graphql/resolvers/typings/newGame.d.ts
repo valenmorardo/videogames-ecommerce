f70a1e8ad81dce9d857dd2ffa7e9caeb045295b6
Valentin <valentin.morardo@gmail.com>
2024-06-06 23:50:45 -0300

    type of new game added

---
+++ api/graphql-service/src/graphql/resolvers/typings/newGame.d.ts
@@ -0,0 +1,12 @@
+export interface INewGame {
+    id: string,
+    name: string,
+    description: string,
+    price: float,
+    released: string,
+    alternative_names: string[],
+    background_image: string,
+    screenshots: string[],
+    publisherId: string,
+    
+}
